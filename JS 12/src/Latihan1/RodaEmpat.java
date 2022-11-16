9effc53dd9f4f0cfb127f8610ad0ba3035669796
Arafil Azmi <112170699+21343019@users.noreply.github.com>
2022-11-16 20:14:17 +0700

    Add files via upload

---
+++ JS 12/src/Latihan1/RodaEmpat.java
@@ -0,0 +1,10 @@
+package Latihan1;
+
+class RodaEmpat extends Kendaraan {
+    double Naikhargake = 8; // 8 kali
+
+    void hargaAkhir(){
+        hargaDasar = hargaDasar + (hargaDasar * Naikhargake);
+        System.out.println("Setelah di modifikasi, harga sepeda menjadi Rp." + hargaDasar);
+    }
+}
